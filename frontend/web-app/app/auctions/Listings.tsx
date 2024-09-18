cf7504fb8a04c669abcab061b24cb8045bc16202
vgita <22777783+vgita@users.noreply.github.com>
2024-09-18 12:33:36 +0300

    display listings

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import AuctionCard from './AuctionCard';
+
+async function getData() {
+	const response = await fetch('http://localhost:6001/search?pageSize=10');
+
+	if (!response.ok) throw new Error('Failed to fetch data');
+
+	return response.json();
+}
+
+export default async function Listings() {
+	const data = await getData();
+
+	return (
+		<div
+			className="
+    grid
+    grid-cols-4
+    gap-6"
+		>
+			{data &&
+				data.results.map((auction: any) => (
+					<AuctionCard auction={auction} key={auction.id} />
+				))}
+		</div>
+	);
+}
