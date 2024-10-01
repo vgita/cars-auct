0a9db8ccd45d8801a219b493839d49dc92d0003a
vgita <22777783+vgita@users.noreply.github.com>
2024-10-01 10:27:03 +0300

    web-app client handle auction created and finished events

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -2,16 +2,22 @@
 
 import { useAuctionStore } from '@/hooks/useAuctionStore';
 import { useBidsStore } from '@/hooks/useBidsStore';
-import { Bid } from '@/types';
+import { Auction, AuctionFinished, Bid } from '@/types';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { User } from 'next-auth';
 import { useParams } from 'next/navigation';
 import { ReactNode, useCallback, useEffect, useRef } from 'react';
+import toast from 'react-hot-toast';
+import AuctionCreatedToast from '../components/AuctionCreatedToast';
+import { getDetails } from '../actions/auctionActions';
+import AuctionFinishedToast from '../components/AuctionFinishedToast';
 
 type Props = {
 	children: ReactNode;
+	user: User | null;
 };
 
-export default function SignalRProvider({ children }: Props) {
+export default function SignalRProvider({ children, user }: Props) {
 	const connection = useRef<HubConnection | null>(null);
 	const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
 	const addBid = useBidsStore((state) => state.addBid);
@@ -30,6 +36,38 @@ export default function SignalRProvider({ children }: Props) {
 		[setCurrentPrice, addBid, params.id]
 	);
 
+	const handleAuctionCreated = useCallback(
+		(auction: Auction) => {
+			if (user?.username !== auction.seller) {
+				return toast(<AuctionCreatedToast auction={auction} />, {
+					duration: 10000
+				});
+			}
+		},
+		[user?.username]
+	);
+
+	const handleAuctionFinished = useCallback(
+		(finishedAuction: AuctionFinished) => {
+			const auction = getDetails(finishedAuction.auctionId);
+			return toast.promise(
+				auction,
+				{
+					loading: 'Loading...',
+					success: (auction) => (
+						<AuctionFinishedToast
+							auction={auction}
+							finishedAuction={finishedAuction}
+						/>
+					),
+					error: () => 'Auction finished'
+				},
+				{ success: { duration: 10000, icon: null } }
+			);
+		},
+		[]
+	);
+
 	useEffect(() => {
 		if (!connection.current) {
 			connection.current = new HubConnectionBuilder()
@@ -44,13 +82,17 @@ export default function SignalRProvider({ children }: Props) {
 		}
 
 		connection.current.on('BidPlaced', handleBidPlaced);
+		connection.current.on('AuctionCreated', handleAuctionCreated);
+		connection.current.on('AuctionFinished', handleAuctionFinished);
 
 		return () => {
 			if (connection.current) {
 				connection.current.off('BidPlaced', handleBidPlaced);
+				connection.current.off('AuctionCreated', handleAuctionCreated);
+				connection.current.off('AuctionFinished', handleAuctionFinished);
 			}
 		};
-	}, [handleBidPlaced]);
+	}, [handleBidPlaced, handleAuctionCreated, handleAuctionFinished]);
 
 	return children;
 }
