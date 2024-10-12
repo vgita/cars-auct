8121beb611b2f18cd062c828f9b9ee40fbbefdec
vgita <22777783+vgita@users.noreply.github.com>
2024-10-12 18:29:01 +0300

    integration tests setup

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -61,3 +61,5 @@ catch (Exception e)
 }
 
 app.Run();
+
+public partial class Program { }
---
+++ tests/AuctionService.IntegrationTests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using Xunit;
+
+global using AuctionService.Data;
+global using AuctionService.Entities;
